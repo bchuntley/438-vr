cbfd066319cd4cab00f894c20e65acfa56771acd
Alex Hicks <alex@alexhicks.net>
2018-04-22 21:42:39 -0400

    Implement basic movement scheme (tilt right controller forward and press the trigger)

--- index.ts
+++ index.ts
@@ -5,24 +5,49 @@ class Game {
     engine: BABYLON.Engine;
     scene: BABYLON.Scene;
     vr: BABYLON.VRExperienceHelper;
-    get controllers() {
-        return this.vr.webVRCamera.controllers;
+    player: BABYLON.Mesh;
+
+    get controllers(): BABYLON.OculusTouchController[] {
+        return <any>this.vr.webVRCamera.controllers;
     }
+
+    isRightTriggerPressed = false;
 
     constructor() {
         this.canvas = <HTMLCanvasElement>document.getElementById("canvas");
         this.engine = new BABYLON.Engine(this.canvas, true);
         this.scene = new BABYLON.Scene(this.engine);
+        this.scene.onBeforeRenderObservable.add(this.beforeRender.bind(this));
         this.vr = this.scene.createDefaultVRExperience({
             controllerMeshes: false
         });
+        this.vr.enableInteractions();
+        this.vr.webVRCamera.onControllersAttachedObservable.add(controllers => {
+            console.log(controllers);
+            this.controllers[1].onTriggerStateChangedObservable.add(evt => {
+                this.isRightTriggerPressed = evt.pressed;
+            });
+        });
         new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), this.scene);
-        const box = BABYLON.MeshBuilder.CreateBox("player", { size: 2 }, this.scene);
+        this.player = BABYLON.MeshBuilder.CreateBox("player", {
+            width: 0.75,
+            height: 0.5,
+            depth: 2
+        }, this.scene);
+        this.player.position = new BABYLON.Vector3(1, 1, 3);
     }
 
     start() {
         this.engine.runRenderLoop(() => this.scene.render());
         window.addEventListener("resize", () => this.engine.resize());
+    }
+
+    beforeRender() {
+        if (this.controllers.length === 0) return;
+        // left is negative z, right is positive z
+        if (this.isRightTriggerPressed) {
+            this.player.position.addInPlace(new BABYLON.Vector3(0, 0, this.controllers[1].deviceRotationQuaternion.z / 10));
+        }
     }
 }
 
